665afb889f8877c664ef3716c8acd75c47db766c
Alfie Powers <powersalfie@gmail.com>
2019-03-26 00:22:33 +0000

    250319v1
    Fixed up some of the possible errors and added some comments explaining the program

--- generateSentence.py
+++ generateSentence.py
@@ -4,10 +4,11 @@ import random
 
 
 def make_dictionary():
-    # Get all of the different words
+    # Retrieve the training sentences from a file
     text = list()
     with open('trainingData/text.txt', 'r') as f:
         for line in f:
+            # Convert the line values into lists of lists of words
             words = list()
             for word in line.split():
                 words.append(word)
@@ -15,24 +16,36 @@ def make_dictionary():
 
     dictionary = defaultdict(list)
 
+    # Add to dictionary should create a new list entry in the case that the first word has not been seen in the
+    # dictionary. It should also add the second word into the dictionary but as the amount of occurances that has
+    # previously been seen + 1 (starting at 0)
+    # If the second word is in the dictionary, we should increment the value once again.
     def add_to_dict(current, next):
+        # Sanity check to ensure that the value is in the dictionary
+        # We could essentially ignore this but it is safer and will lead to less type errors
         if current in dictionary:
             li = dictionary[current]
+
+            # Our implementation of is in list. As the list is not ordered we should assume that the best we can do
+            # is an implementation that loops through the entire list. Could order this with a hashmap but potential
+            # memory overhead plus this dictionary need only be generated once.
             in_list = False
             for x in range(0, len(li), 1):
                 if li[x][0] == next:
                     in_list = True
+
+            # Checks if the second word is in the list, incrementing if it is, else adding a 1 entry.
             if in_list:
                 for x in range(0, len(li), 1):
                     if li[x][0] == next:
                         li[x] = (next, li[x][1] + 1)
                 dictionary[current] = li
             else:
-                dictionary[current].append((next, 0))
+                dictionary[current].append((next, 1))
         else:
-            dictionary[current].append((next, 0))
+            dictionary[current].append((next, 1))
 
-    # Process the words into a dictionary
+    # We loop through all of the words and add to the dictionary with the next word if it is not blank
     for l in text:
         for w in range(0, len(l), 1):
             if w == len(l) - 1:
@@ -43,11 +56,19 @@ def make_dictionary():
     np.save('output/dictionary.npy', dictionary)
 
 
+# This generates the sentence given a starting word
 def generate_sentence():
-    read_dictionary = np.load('output/dictionary.npy').item()
+    # Reads the dictionary from memory ensuring it exists
+    try:
+        read_dictionary = np.load('output/dictionary.npy').item()
+    except FileNotFoundError:
+        make_dictionary()
+        read_dictionary = np.load('output/dictionary.npy').item()
 
+    # This function given a word will find the next possible word in the sequence. Done by generating a list of all
+    # of the possible words with words repeated given how often they show up
     def get_next_word(previous, sentence):
-
+        # Ensure that there is a next word to be read
         if previous != ".":
             sentence = sentence + " " + previous
             possible_words = list()
@@ -55,17 +76,21 @@ def generate_sentence():
                 for y in range(0, x[1] + 1, 1):
                     possible_words.append(x[0])
             next_word = random.choice(possible_words)
+            # Recursively call until we have met the condition of an end word
             return get_next_word(next_word, sentence)
         sentence = sentence
         return sentence
 
+    # A base starting word
     starting_word = "america"
+
+    # We need to specify the starting word and the current sentence
     try:
         sentence = get_next_word(starting_word, "")
     except IndexError:
+        # If we can't find a word in the dictionary, we can't start. There is no training data
+        # TODO: Implement a way of finding the next closest word to the current value
         return "Failed to find starting word: " + starting_word
     return sentence
 
-
-make_dictionary()
 print(generate_sentence())
